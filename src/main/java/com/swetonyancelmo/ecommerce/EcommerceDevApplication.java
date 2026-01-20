5c293b8f128f32de41a4b0896ab4201f3af7f5b9
Swetony <swetonyancelmo@gmail.com>
2026-01-20 17:14:26 -0300

    chore: commit inicial e configuração do banco de dados via docker

---
+++ src/main/java/com/swetonyancelmo/ecommerce/EcommerceDevApplication.java
@@ -0,0 +1,13 @@
+package com.swetonyancelmo.ecommerce;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EcommerceDevApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EcommerceDevApplication.class, args);
+	}
+
+}
